a4811318ffba88a6985b6784050386457524d3de
sami102 <samcios102@gmail.com>
2019-08-25 16:14:51 +0200

    exercise preparedStatement

--- src/main/java/DatabaseApp.java
+++ src/main/java/DatabaseApp.java
@@ -10,23 +10,32 @@ public class DatabaseApp {
         Class.forName("org.postgresql.Driver");
 
         Connection connection = DriverManager.getConnection(JDBC_URL, DATABASE_USER, DATABASE_PASS);
-        Statement statement = connection.createStatement();
-
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM notes");
+//        Statement statement = connection.createStatement();
+//
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM notes");
 //        int i = 0;
 //        while ((resultSet.next())){
 //            i++;
 //            System.out.println("Kursor aktualnie jest na " + i + " wierszu.");
 //        }
 
-        while ( (resultSet.next())){
-            int noteId = resultSet.getInt(1);
-            String noteTitle = resultSet.getString("note_title");
-            String noteMsg = resultSet.getString(3);
-            int userId = resultSet.getInt("user_id");
+//        while ( (resultSet.next())){
+//            int noteId = resultSet.getInt(1);
+//            String noteTitle = resultSet.getString("note_title");
+//            String noteMsg = resultSet.getString(3);
+//            int userId = resultSet.getInt("user_id");
+//
+//            System.out.println("Id notatki: " +noteId+ ",tytuł: " + noteTitle+ ",treść:" +noteMsg+ ",id użytkownika: " +userId);
+//        }
 
-            System.out.println("Id notatki: " +noteId+ ",tytuł: " + noteTitle+ ",treść:" +noteMsg+ ",id użytkownika: " +userId);
-        }
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO notes (note_id, note_title, note_message, user_id) VALUES (?, ?, ?, ?);");
+
+        preparedStatement.setInt(1, 123);
+        preparedStatement.setString(2, "aaa");
+        preparedStatement.setString(3, "xyz");
+        preparedStatement.setInt(4, 2);
+
+        preparedStatement.executeUpdate();
 
 
 //        boolean resultFlag = statement.execute("INSERT INTO users (user_id, user_nick, user_name) VALUES (100, 'kowal123', 'Jan');");
@@ -38,7 +47,7 @@ public class DatabaseApp {
 
 
 
-        statement.close();
+        preparedStatement.close();
         connection.close();
     }
 
