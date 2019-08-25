a6c2d857b5a9b9b8b7410371454bb01e2fd08eef
sami102 <samcios102@gmail.com>
2019-08-25 15:13:29 +0200

    exercise

---
+++ src/main/java/DatabaseApp.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class DatabaseApp {
+
+    private static String JDBC_URL = "jdbc:postgresql://localhost:5433/notesdb";
+    private static String DATABASE_USER = "postgres";
+    private static String DATABASE_PASS = "programator";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        Connection connection = DriverManager.getConnection(JDBC_URL, DATABASE_USER, DATABASE_PASS);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM notes");
+
+        boolean resultFlag = statement.execute("INSERT INTO users (user_id, user_nick, user_name) VALUES (100, 'kowal123', 'Jan');");
+        if (!resultFlag){
+            System.out.println("Ilość dodanych rekordów: + " + statement.getUpdateCount());
+        }
+
+        statement.close();
+        connection.close();
+    }
+
+
+}
